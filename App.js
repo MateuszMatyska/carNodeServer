0a1fbba59d5fdc5d62f3ad27d7e552430e92e2f2
Mateusz Matyska <mateuszmatyska@gmail.com>
2021-03-29 19:05:37 +0200

    auth routers

--- App.js
+++ App.js
@@ -5,10 +5,19 @@ const config = require('./config');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
+const users = require('./routes/auth/authRoute');
 const cars = require('./routes/cars/carsRoute');
+
 
 app.use(cors());
 app.use(bodyParser.json());
+
+// app.use(function (req, res, next) {
+//     console.log('Time:', Date.now())
+//     next()
+// })
+
+app.use('/auth',users);
 app.use('/cars',cars);
 
 mongoose.connect(config.dbUrlCars, {useNewUrlParser: true}, () => console.log('connected to DB'));
